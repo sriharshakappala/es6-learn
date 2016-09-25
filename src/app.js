f2c5a5b150cb547e5030d85d5919a5c3ab8ebb7a
Sri Harsha Kappala <sriharsha.kappala@hotmail.com>
2016-09-25 09:07:22 +0530

    Static Method in ES6

--- src/app.js
+++ src/app.js
@@ -7,6 +7,11 @@ class Drone {
   fly() {
     console.log('Drone ' + this.id + ' is flying.');
   }
+
+  // Static Method
+  static getCompany() {
+    console.log('In getCompany');
+  }
 }
 
 let drone = new Drone('A123', 'Flyer')
@@ -14,3 +19,5 @@ let drone2 = new Drone('B456', 'Twirl')
 
 drone.fly();
 drone2.fly();
+Drone.getCompany();
+drone.getCompany(); // throws error
