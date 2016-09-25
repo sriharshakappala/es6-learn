4cfd0dcc77f5d5df035ae642cf740f2d9e0fa0f6
Sri Harsha Kappala <sriharsha.kappala@hotmail.com>
2016-09-25 08:34:49 +0530

    Define class and object in ES6 syntax

---
+++ src/app.js
@@ -0,0 +1,19 @@
+class Drone {
+  constructor(id) {
+    console.log('in drone constructor');
+    console.log('id' + id);
+  }
+}
+
+let drone = new Drone('A123')
+
+// console.log(typeof Drone);
+// console.log(typeof drone);
+// console.log(drone instanceof Drone);
+
+// Init
+// console.log('in app.js')
+
+// Strict Mode
+// let droneId = 5;
+// console.log(window.droneId)
