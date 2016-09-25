31eccad133302a6a423ce7b75ae99eefe6372989
Sri Harsha Kappala <sriharsha.kappala@hotmail.com>
2016-09-25 09:03:31 +0530

    Methods in ES6 class

--- src/app.js
+++ src/app.js
@@ -3,23 +3,14 @@ class Drone {
     this.id = id;
     this.name = name;
   }
+
+  fly() {
+    console.log('Drone ' + this.id + ' is flying.');
+  }
 }
-// Static Properties or Class Properties
-Drone.maxHeight = 2000;
 
 let drone = new Drone('A123', 'Flyer')
 let drone2 = new Drone('B456', 'Twirl')
-console.log(drone.id + ' ' + drone2.id);
-console.log(Drone.maxHeight);
-console.log(drone.maxHeight);
 
-// console.log(typeof Drone);
-// console.log(typeof drone);
-// console.log(drone instanceof Drone);
-
-// Init
-// console.log('in app.js')
-
-// Strict Mode
-// let droneId = 5;
-// console.log(window.droneId)
+drone.fly();
+drone2.fly();
