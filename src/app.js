f5cbf767483b73ca55b309a158a4996bd9a79ad6
Sri Harsha Kappala <sriharsha.kappala@hotmail.com>
2016-09-27 08:08:47 +0530

    Sample filter in DataService

--- src/app.js
+++ src/app.js
@@ -7,20 +7,7 @@ import {FleetDataService} from './services/fleet-data-service.js'
 let dataService = new FleetDataService()
 dataService.loadData(fleet);
 
-console.log('Get car by License');
-let car = dataService.getCarByLicense('AT2000')
-console.log(car)
+let cars = dataService.filterCarsByMake('e');
 
-console.log('Sort Function');
-let cars = dataService.getCarsSortedByLicense();
-for (let car of cars)
-  console.log(car.license);
-
-// for (let car of dataService.cars)
-//   console.log(car.license);
-//
-// for (let drone of dataService.drones)
-//   console.log(drone.license);
-//
-// for (let e of dataService.errors)
-//   console.log(e.message);
+for(let car of cars)
+  console.log(car.make);
