53b96d6e91127a5d2ceb6f0472d1c1d72df45352
Sri Harsha Kappala <sriharsha.kappala@hotmail.com>
2016-09-30 08:08:57 +0530

    DataTable class

--- src/app.js
+++ src/app.js
@@ -8,6 +8,13 @@ import {FleetDataService} from './services/fleet-data-service.js'
 import {Button} from './ui/button.js'
 import {Image} from './ui/image.js'
 import {TitleBar} from './ui/title-bar.js'
+import {DataTable} from './ui/data-table.js'
+
+let headers = 'License Make Model Miles'.split(' ');
+let dataService = new FleetDataService();
+dataService.loadData(fleet);
+let dt = new DataTable(headers, dataService.cars);
+dt.appendToElement($('body'));
 
 // let dataService = new FleetDataService()
 // dataService.loadData(fleet);
@@ -17,12 +24,12 @@ import {TitleBar} from './ui/title-bar.js'
 // for(let car of cars)
 //   console.log(car.make);
 
-let tb = new TitleBar('Our Application');
-tb.addLink('Home', '');
-tb.addLink('Cars', '');
-tb.addLink('Drones', '');
-tb.addLink('Map', '');
-tb.appendToElement($('body'));
+// let tb = new TitleBar('Our Application');
+// tb.addLink('Home', '');
+// tb.addLink('Cars', '');
+// tb.addLink('Drones', '');
+// tb.addLink('Map', '');
+// tb.appendToElement($('body'));
 
 // let b = new Button('Click Me');
 // b.appendToElement($('body'));
